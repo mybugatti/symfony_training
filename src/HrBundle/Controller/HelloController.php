145b6bb44bbde0a6e78902e82a06fb79403d0f2c
mybugatti <ngoma.heredia@gmail.com>
2016-11-12 23:32:35 +0100

    # create Entity Book / Doctrine

--- src/HrBundle/Controller/HelloController.php
+++ src/HrBundle/Controller/HelloController.php
@@ -3,12 +3,25 @@
 namespace HrBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name,$sexe)
     {
+
+        if($sexe == "h") {
+            $sexe = "masculin";
+            $qualification = "Monsieur";
+        } else {
+            $sexe = "feminin";
+            $qualification = "Madame";
+        }
         return $this->render('HrBundle:Hello:index.html.twig',
-            array('name' => $name));
+            array('name' => $name, 'sexe' => $sexe, 'qualification'=>$qualification));
+    }
+
+    public function helloAction($name) {
+        return new Response('Hello ' . $name . ', you are in the "hello" action (controller)');
     }
 }
